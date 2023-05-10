51dcbfa809a400c65c2660abda1fc398bb10e7e6
long bin <david.lb90@gmail.com>
2023-05-10 10:04:45 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const deliveroo = document.getElementById("deliveroo");
+const surplace = document.getElementById("surplace");
+const lienMenu = document.getElementById("indexVersMenu");
+
+lienMenu.addEventListener("click", function () {
+  window.location.replace("../pages/menu.html");
+});
+
+deliveroo.addEventListener("click", function () {
+  window.open(
+    "https://deliveroo.fr/fr/menu/Strasbourg/la-petite-france/restaurant-il-girasole?day=today&geohash=u0ts26sbt7bh&time=1915",
+    "_blank"
+  );
+});
+
+surplace.addEventListener("click", function () {
+  window.location.replace("../pages/menu.html");
+});
---
+++ scripts/menu.js
@@ -0,0 +1,56 @@
+let menuData = []; // array pour stocker la data des works
+let categoryData = []; // array pour stocker les data des categories
+const antipasti = document.getElementById("antipasti");
+const carpaccio = document.getElementById("carpaccio");
+const insalate = document.getElementById("insalate");
+const specialite = document.getElementById("specialite");
+const pesce = document.getElementById("pesce");
+const carne = document.getElementById("carne");
+const pasta = document.getElementById("pasta");
+const risotto = document.getElementById("risotto");
+const pizza = document.getElementById("pizza");
+const dessert = document.getElementById("dessert");
+const retourAcceuil = document.getElementById("menuVersAcceuil");
+const reservation = document.getElementById("menuVersRéservation");
+
+retourAcceuil.addEventListener("click", function () {
+  window.location.replace("../index.html");
+});
+
+const getMenu = async () => {
+  await fetch("https://sheetdb.io/api/v1/rx6fpue4d9jt1")
+    .then((res) => res.json())
+    .then((data) => (menuData = data));
+  console.log(menuData);
+};
+
+const generateMenu = (element, item) => {
+  if (item.category === element.getAttribute("id")) {
+    element.innerHTML += `
+      <div class="card">
+      <h3>${item.name}</h3>
+      <p> ${item.description} </p>
+      <h3> ${item.price}</h3>
+      </div>
+  
+      `;
+  }
+};
+
+const showFilteredMenu = async () => {
+  await getMenu();
+  menuData.forEach(function (item) {
+    generateMenu(antipasti, item);
+    generateMenu(carpaccio, item);
+    generateMenu(insalate, item);
+    generateMenu(specialite, item);
+    generateMenu(pesce, item);
+    generateMenu(carne, item);
+    generateMenu(pasta, item);
+    generateMenu(risotto, item);
+    generateMenu(pizza, item);
+    generateMenu(dessert, item);
+  });
+};
+
+showFilteredMenu();
